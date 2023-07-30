9923063ffd0fdadbf8299512a5a3c385d2ec01e7
Manuel Nila <3621543+manila@users.noreply.github.com>
2023-07-29 22:22:45 -0400

    gensite.sh: add webmention linkrel

--- gensite.sh
+++ gensite.sh
@@ -14,6 +14,7 @@ TOP_BUN='<!doctype html>
 <head>
 <link rel="me" href="https://mastodon.social/@manila" />
 <link rel="me" href="mailto:manila@manila.me" />
+<link rel="webmention" href="https://webmention.io/manila.me/webmention" />
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-GLHGR2F8XD"></script>
 <script>
